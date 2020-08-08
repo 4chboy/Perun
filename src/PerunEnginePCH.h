e13c8cf11e24cee618b5f9771e2b784cf632dbc5
Andrew Shkrob <andrew.shkrob.social@yandex.by>
2020-08-09 00:43:21 +0300

    reorganized file structure

---
+++ src/PerunEnginePCH.h
@@ -0,0 +1,31 @@
+#ifndef _PERUNENGINE_PCH_H
+#define _PERUNENGINE_PCH_H
+
+#include <cstdint>
+#include <memory>
+#include <functional>
+
+#include <string>
+#include <array>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <deque>
+#include <stack>
+
+#ifdef PERUNENGINE_OS_WINDOWS
+    #include <Windows.h>
+    #include <Windowsx.h>
+
+    #ifdef PERUNENGINE_BUILD_DLL
+        #define PERUNENGINE_API __declspec(dllexport)
+    #else
+        #define PERUNENGINE_API __declspec(dllimport)
+    #endif
+#else
+    #define PERUNENGINE_API
+#endif
+
+#endif //_PERUNENGINE_PCH_H
